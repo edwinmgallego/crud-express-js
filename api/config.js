25b4130c3e4c5e7be32000a7a852cf765d4d64d3
edwinmgallego <ing.edwin.gallego@gmail.com>
2024-11-29 23:07:21 -0500

    arreglando cositas subiendo a railway4  packaje.json start script

--- api/config.js
+++ api/config.js
@@ -1,6 +1,14 @@
-export  const PORT = process.env.PORT || 3000
+/*export  const PORT = process.env.PORT || 3000
 export  const DB_HOST = process.env.DB_HOST || 'localhost'
 export  const DB_USER = process.env.DB_USER || 'root'
 export  const DB_PASSWORD = process.env.DB_PASSWORD || ''
 export  const DB_NAME = process.env.DB_NAME || 'agenda'
-export  const DB_PORT = process.env.DB_PORT || 3306
+export  const DB_PORT = process.env.DB_PORT || 3306*/
+module.exports = {
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_USER: process.env.DB_USER,
+    DB_PORT: process.env.DB_PORT,
+    PORT: process.env.PORT || 3000
+  };
