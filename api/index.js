2888d43d61675f130a9542004a0dfd1fd6b95048
edwinmgallego <ing.edwin.gallego@gmail.com>
2024-11-29 22:39:10 -0500

    arreglando cositas subiendo a railway

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
+import { DB_HOST,DB_NAME,DB_PASSWORD,DB_USER,DB_PORT } from "./config";
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2/promise");
 
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT|| 3306;
 
 // Middleware
 app.use(cors());
@@ -12,16 +15,17 @@ app.use(bodyParser.json());
 
 // Conexión a la base de datos
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "", // Ajusta según tu configuración
-  database: "todo_app",
+  host:DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD, // Ajusta según tu configuración
+  database: DB_NAME,
+  port : DB_PORT 
 });
 
 // Obtener todas las tareas
 app.get("/todos", async (req, res) => {
   try {
-    const [rows] = await pool.query("SELECT * FROM todos");
+    const [rows] = await pool.query("SELECT * FROM todo");
     res.json(rows);
   } catch (error) {
     console.error(error);
@@ -34,7 +38,7 @@ app.post("/todos", async (req, res) => {
   const { text } = req.body;
   try {
     const [result] = await pool.query(
-      "INSERT INTO todos (text, completed) VALUES (?, ?)",
+      "INSERT INTO todo (text, completed) VALUES (?, ?)",
       [text, false]
     );
     res.json({ id: result.insertId });
@@ -51,10 +55,10 @@ app.put("/todos/:id", async (req, res) => {
 
   try {
     if (text !== undefined) {
-      await pool.query("UPDATE todos SET text = ? WHERE id = ?", [text, id]);
+      await pool.query("UPDATE todo SET text = ? WHERE id = ?", [text, id]);
     }
     if (completed !== undefined) {
-      await pool.query("UPDATE todos SET completed = ? WHERE id = ?", [
+      await pool.query("UPDATE todo SET completed = ? WHERE id = ?", [
         completed,
         id,
       ]);
@@ -71,7 +75,7 @@ app.delete("/todos/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
-    await pool.query("DELETE FROM todos WHERE id = ?", [id]);
+    await pool.query("DELETE FROM todo WHERE id = ?", [id]);
     res.json({ message: "Tarea eliminada correctamente" });
   } catch (error) {
     console.error(error);
@@ -81,5 +85,5 @@ app.delete("/todos/:id", async (req, res) => {
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en ${PORT}`);
 });
